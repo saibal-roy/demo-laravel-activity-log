ad3313cb5cb0f2dc0caba1d04d3c738bd57f5720
Technoexponent <sunnyroy21@gmail.com>
2020-02-17 19:31:50 +0530

    add activity log package with causer

--- app/Article.php
+++ app/Article.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Article extends Model
 {
-    //
+    use LogsActivity;
+
+    protected $fillable = ['title'];
+
+    protected static $logAttributes = ['title'];
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,18 @@
 */
 
 Route::get('/', function () {
+    /*
+    |---------------------------------------------
+    |After adding the CausesActivity trait in the Users table
+    |it logs the causer in the activity log table.
+    |For the other models that needs to be logged use the LogsActivity table
+    */
+    auth()->loginUsingId(1);
+    $article = \App\Article::find(1);
+    $article->title = "test" . time();
+    $article->save();
+
+
     return view('welcome');
 });
 
